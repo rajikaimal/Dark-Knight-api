bccb68dc5d32ef40772a8e237f4eb882145dcbea
Rajika <inbox.rajika@gmail.com>
2015-06-13 23:10:08 +0530

    Added Router

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
+var port = 3000;
+
+app.use('/api',router);
+
+router.route('/batwing')
+  .get(function(req,res){
+    
+  });
+
+router.route('/batgadgets')
+  .get(function(req,res){
+    
+  });
 
 app.get('/',function(req,res){
   res.send('API home');
 });
 
-app.listen(3000);
+app.listen(port,function(){
+  console.log('Server listening on port ' + port);
+});
