b00c0d97ad3311f7279922c39fce7658520efd1c
Rajika <inbox.rajika@gmail.com>
2015-06-14 10:07:56 +0530

    Middleware

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+'use strict'
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var port = 3000;
 var db = mongoose.connect('mongodb://localhost/batman');
+
+app.use(bodyParser.json());
 
 var batModel = require('./models/batModel.js');
 var router = require('./routes/batRoutes.js')(batModel);
 
 app.use('/api',router);
-
-
 
 app.get('/',function(req,res){
   res.send('API home');
